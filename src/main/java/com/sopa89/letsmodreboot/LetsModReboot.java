7c815a27ad2fd46e2015bf52b7093d2c90376639
timcampbell <tim.campbell127@gmail.com>
2014-06-26 18:11:42 -0400

    Created proxy object

--- src/main/java/com/sopa89/letsmodreboot/LetsModReboot.java
+++ src/main/java/com/sopa89/letsmodreboot/LetsModReboot.java
@@ -1,6 +1,9 @@
 package com.sopa89.letsmodreboot;
 
+import com.sopa89.letsmodreboot.proxy.IProxy;
+
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -10,6 +13,9 @@ public class LetsModReboot
 {
 	@Mod.Instance("LetsModReboot")
 	public static LetsModReboot instance;
+	
+	@SidedProxy(clientSide="com.sopa89.letsmodreboot.proxy.ClientProxy", serverSide="com.sopa89.letsmodreboot.proxy.ServerProxy")
+	public static IProxy proxy;
 	
 	@Mod.EventHandler()
 	public void preInit(FMLPreInitializationEvent event)
