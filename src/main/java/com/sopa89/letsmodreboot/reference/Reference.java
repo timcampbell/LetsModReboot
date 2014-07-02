bd031fdc239b5cfbe5b090148829a9825c8a3d38
timcampbell <tim.campbell127@gmail.com>
2014-07-02 15:31:05 -0400

    Updated to forge 1.7.10-10.13.0.1160 and Minecraft 1.7.10

--- src/main/java/com/sopa89/letsmodreboot/reference/Reference.java
+++ src/main/java/com/sopa89/letsmodreboot/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	public static final String MOD_ID="LetsModReboot";
 	public static final String MOD_NAME="Lets Mod Reboot";
-	public static final String VERSION="1.7.2-1.0";
+	public static final String VERSION="1.7.10-1.0";
 	
 	public static final String CLIENT_PROXY_CLASS="com.sopa89.letsmodreboot.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS="com.sopa89.letsmodreboot.proxy.ServerProxy";
