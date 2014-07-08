3d7295865f9c2cc7cb45dd2d0c716511f9e28cdc
timcampbell <tim.campbell127@gmail.com>
2014-07-08 15:42:22 -0400

    Added in basic flag block

--- src/main/java/com/sopa89/letsmodreboot/init/ModBlocks.java
+++ src/main/java/com/sopa89/letsmodreboot/init/ModBlocks.java
@@ -2,9 +2,11 @@ package com.sopa89.letsmodreboot.init;
 
 import com.sopa89.letsmodreboot.block.BlockFlag;
 import com.sopa89.letsmodreboot.block.BlockLMRB;
+import com.sopa89.letsmodreboot.reference.Reference;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 
+@GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModBlocks
 {
 	public static final BlockLMRB flag=new BlockFlag();
