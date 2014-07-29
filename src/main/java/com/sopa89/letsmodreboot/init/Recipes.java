0e48fe0c86630a37c68d338b3f26522a10b789c6
timcampbell <tim.campbell127@gmail.com>
2014-07-28 22:27:14 -0400

    Added ore recipes for ItemMapleLeaf and BlockCanadianFlag

--- src/main/java/com/sopa89/letsmodreboot/init/Recipes.java
+++ src/main/java/com/sopa89/letsmodreboot/init/Recipes.java
@@ -2,13 +2,15 @@ package com.sopa89.letsmodreboot.init;
 
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class Recipes
 {
 	public static void init()
 	{
-		GameRegistry.addRecipe(new ItemStack(ModItems.mapleLeaf), " s ", "sss", " s ", 's', new ItemStack(Items.stick));
-		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.flag), new ItemStack(ModItems.mapleLeaf), new ItemStack(ModItems.mapleLeaf));
+		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.mapleLeaf), " s ", "sss", " s ", 's', "stickWood"));
+		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ModBlocks.flag), new ItemStack(ModItems.mapleLeaf), new ItemStack(ModItems.mapleLeaf)));
 	}
 }
